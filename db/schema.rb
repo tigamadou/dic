3d2e7c7b7a63aefd80693d241ae688f8494d6096
Amadou <amadou140589@gmail.com>
2021-10-06 03:10:13 +0100

    seacrh indexes added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_004121) do
+ActiveRecord::Schema.define(version: 2021_10_06_020818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2021_10_06_004121) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "priority", default: "low"
+    t.index ["expired_at"], name: "index_tasks_on_expired_at"
+    t.index ["name"], name: "index_tasks_on_name"
+    t.index ["status"], name: "index_tasks_on_status"
   end
 
 end
