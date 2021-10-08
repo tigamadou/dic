266278d55cd6eb912302f5e17d3f89116ec9eb1d
Amadou <amadou140589@gmail.com>
2021-10-08 14:02:45 +0100

    seeder updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,25 +11,35 @@ password = "123456"
 user = User.create!(name: name, email: email,password: password, role: 'admin')
 user.tasks.create(name: 'First Task', content: 'This the first task content', expired_at: DateTime.now, status:'unstarted', priority: 'low')
 
-['Todo', 'Doing', 'Done'].each do |tag|
-  Tag.create(title: tag);
-end
-
-tags = [1,2,3]
 
 10.times do |y|
-    status= ['unstarted','in_progress','completed']
-    priorities= ['low','medium','hight']
-    task = user.tasks.create(
-                        name: Faker::Lorem.sentence,
-                        content: Faker::Lorem.paragraph,
-                        expired_at: DateTime.now,
-                        status: status.sample
-                        )
-    Tagging.create(tag_id: tags.sample, task_id: task.id)
-    
-    
+  Tag.create(title: Faker::Lorem.word);
+  User.create(
+    name: Faker::Games::Pokemon.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    role: 'default'
+  )
 end
+tags = [1,2,3]
+
+User.all.each do |user|
+
+  10.times do |y|
+      status= ['unstarted','in_progress','completed']
+      priorities= ['low','medium','hight']
+      task = user.tasks.create(
+                          name: Faker::Lorem.sentence,
+                          content: Faker::Lorem.paragraph,
+                          expired_at: DateTime.now,
+                          status: status.sample
+                          )
+      Tagging.create(tag_id: Tag.all.pluck(:id).sample, task_id: task.id)
+      
+      
+  end
+end
+
 
 
 
